7def920e2b4443bd4c89ebfd74b1fb8f4105f407
Antonio Garrote <antonio.garrote@fundingcircle.com>
2015-06-30 16:40:10 +0100

    Better way of storing constraints

--- lib/languages/poly_featherweight_ruby/types/polymorphism/type_variable_register.rb
+++ lib/languages/poly_featherweight_ruby/types/polymorphism/type_variable_register.rb
@@ -17,41 +17,41 @@ module TypedRb
 
             def type_variable_for(type, variable, hierarchy)
               ensure_string(variable)
-              type_var = hierarchy.detect do |ruby_type|
+              class_for_type_var = hierarchy.detect do |ruby_type|
                 type_variables_register[[type, ruby_type, variable]]
               end
-
-              type_var = if type_var.nil?
-                           new_var_name = "#{hierarchy.first}:#{variable}"
-                           TypeVariable.new(new_var_name)
-                         else
-                           type_variables_register[[type, type_var, variable]]
-                         end
-              type_variables_register[[type, hierarchy.first, variable]] = type_var
-              type_var
+              if class_for_type_var.nil?
+                new_var_name = "#{hierarchy.first}:#{variable}"
+                type_var = TypeVariable.new(new_var_name)
+                type_variables_register[[type, hierarchy.first, variable]] = type_var
+                type_var
+              else
+                type_variables_register[[type, class_for_type_var, variable]]
+              end
             end
 
             def type_variable_for_message(variable, message)
               ensure_string(variable)
-              new_var_name = "#{variable}:#{message}"
-              type_var = type_variables_register[[:return, new_var_name]]
+              key = [:return, message, variable]
+              type_var = type_variables_register[key]
               if type_var.nil?
+                new_var_name = "#{variable}:#{message}"
                 type_var = TypeVariable.new(new_var_name)
-                type_variables_register[[:return, new_var_name]] = type_var
+                type_variables_register[key] = type_var
               end
               type_var
             end
 
             def type_variable_for_abstraction(abs_kind, variable, context)
               ensure_string(variable)
-              new_var_name = "TV_#{context.context_name}:#{abs_kind}:#{variable}"
-              new_var_name = Model::GenSym.next(new_var_name)
-              type_var = type_variables_register[[abs_kind.to_sym, new_var_name]]
+              key = [abs_kind.to_sym, context.context_name, variable]
+              type_var = type_variables_register[key]
               if type_var.nil?
                 # don't gnerate a random fresh name for the var, use the one we're
                 # providing
+                new_var_name = "TV_#{context.context_name}:#{abs_kind}:#{variable}"
                 type_var = TypeVariable.new(new_var_name, gen_name: false)
-                type_variables_register[[abs_kind.to_sym, new_var_name]] = type_var
+                type_variables_register[key] = type_var
               end
               type_var
             end
@@ -91,17 +91,9 @@ module TypedRb
 
             def rename_constraints(constraints, type_variable_mapping)
               constraints.map do |constraint|
-                if constraint.size == 2
-                  type, variable = constraint
-                  new_variable = type_variable_mapping[variable] || variable
-                  [type, new_variable]
-                elsif constraint.size == 3
-                  type, hierachy, variable = constraint
-                  new_variable = type_variable_mapping[variable] || variable
-                  [type, hierachy, new_variable]
-                else
-                  fail StandardError, "Unknown type of constraint #{constraint.to_s}"
-                end
+                type, info, variable = constraint
+                new_variable = type_variable_mapping[variable] || variable
+                [type, info, new_variable]
               end
             end
 
