0a9102d6141cbc9d97e963e39a77ccd38c5f2afb
Antonio Garrote Hernández <antonio@Antonios-MacBook-Air.local>
2015-07-11 22:56:43 +0100

    Adding additional example for generic type instantiation.

--- spec/lib/languages/poly_featherweight_ruby/typing/tm_send_generic_apply_spec.rb
+++ spec/lib/languages/poly_featherweight_ruby/typing/tm_send_generic_apply_spec.rb
@@ -31,5 +31,42 @@ __CODE
       result = language.check(code)
       expect(result.type_vars[0].ruby_type).to eq(Integer)
     end
+
+    it 'captures type errors in the materialization' do
+
+      code = <<__CODE
+        ts 'type Pod2[X<Numeric]'
+        class Pod2
+
+          ts '#initialize / -> unit'
+          def initialize
+             @value = base
+          end
+
+          ts '#base / -> Float'
+          def base
+            1.0
+          end
+
+          ts '#get / -> [X]'
+          def get
+            @value
+          end
+
+          ts '#put / [X] -> unit'
+          def put(x)
+            @value = x
+          end
+
+        end
+__CODE
+
+      expect {
+        language.check("#{code}; Pod2.(Integer)")
+      }.to raise_error(StandardError)
+
+      result = language.check("#{code}; Pod2.(Float)")
+      expect(result.type_vars[0].ruby_type).to eq(Float)
+    end
   end
 end
