3e1158a2205780f678299a142fcc4dd69538cb08
Antonio Garrote Hernández <antonio@Antonios-MacBook-Air.local>
2015-07-28 20:24:05 +0100

    Adding specs for nested generic types.

--- spec/lib/types/ty_generic_object_spec.rb
+++ spec/lib/types/ty_generic_object_spec.rb
@@ -45,4 +45,33 @@ __CODE
       language.check(expr)
     }.to raise_error(TypedRb::Types::UncomparableTypes,'Cannot compare types String <=> Class[Integer]')
   end
+
+  it 'correctly types nested generic types' do
+    expr = <<__CODE
+       x = Array.('Array[Integer]').new
+       x << Array.(Integer).new
+       y = x.at(0)
+       y << 1
+       y.at(0)
+__CODE
+
+    result = language.check(expr)
+    expect(result.ruby_type).to eq(Integer)
+  end
+
+  it 'detects errors in the types of nested generic types' do
+
+    expr = <<__CODE
+       x = Array.('Array[Integer]').new
+       x << Array.(String).new
+       y = x.at(0)
+       y << 1
+       y.at(0)
+__CODE
+
+    expect {
+      language.check(expr)
+    }.to raise_error(TypedRb::Types::UncomparableTypes,'Cannot compare types Class[Integer] <=> Class[String]')
+
+  end
 end
