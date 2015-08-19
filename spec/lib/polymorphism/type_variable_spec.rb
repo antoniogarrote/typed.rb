78fe0f50057c4654fd3dc8f62134c69085647dd0
Antonio Garrote <antoniogarrote@gmail.com>
2015-08-19 17:14:53 +0100

    fixing spec for type variable, using context to generate var

--- spec/lib/polymorphism/type_variable_spec.rb
+++ spec/lib/polymorphism/type_variable_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../spec_helper'
 describe TypedRb::Types::Polymorphism::TypeVariable do
   it 'should record constraints' do
     type = tyobject(Integer)
-    type_var = described_class.new('A::@a')
+    type_var = TypedRb::Types::TypingContext.type_variable_for(:test, '@a', [Object])
     type_var.compatible?(type)
     expect(type_var.constraints).to eq([[type_var, :lt, type]])
 
