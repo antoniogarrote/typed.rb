b46ef557e25fadbf26dcf18e1b602eeb622afb1e
Antonio Garrote Hernández <antonio@Antonios-MacBook-Air.local>
2015-08-27 22:05:58 +0100

    Refactoring signature parser.

--- lib/type_signature/parser.rb
+++ lib/type_signature/parser.rb
@@ -6,108 +6,56 @@ module TypedRb
       end
     end
 
-    class Parser
-
-      def self.parse(expr, method_info = {})
-        (@parser || Parser.new(method_info)).parse(expr)
-      end
-
-      attr_reader :method_info
-
-      def initialize(method_info = {})
-        @method_info = method_info
-        @current_type = []
-        @current_function = []
-        @stack = []
-        @in_binding = false
-      end
-
-      def parse (expr)
-        expr = expr.gsub(/\s+/,'').gsub('->','>')
-
-        expr.each_char do |elem|
-          if elem == '('
-            parse_start_of_type
-          elsif elem == '['
-            @in_binding  = true
-            parse_start_of_type
-          elsif elem == ')'
-            parse_end_of_function
-          elsif elem == '<'
-            @binding = '<'
-            parse_binding
-          elsif elem == ']'
-            @in_binding  = false
-            parse_end_of_binding
-          elsif elem == '>' && @in_binding == false
-            parse_next_elem
-          elsif elem == '>' && @in_binding == true
-            @binding = '>'
-            parse_binding
+    module TokenProcessor
+      def group_tokens(tokens)
+        next_group = []
+        groups = [next_group]
+        tokens.each do |token|
+          if token == :<
+            next_group = []
+            groups << next_group
           else
-            @current_type << elem
+            next_group << token
           end
         end
+        groups
+      end
 
-        unless @stack.empty?
-          fail ParsingError.new(expr, 'Unbalanced parentheses.')
-        end
+      def transform_function_tokens(tokens)
+        group_tokens(tokens).map do |token_group|
+          if token_group.is_a?(Array)
+            transform_nested_function(token_group)
+          elsif token_group.is_a?(String)
+            token_group
+          end
+        end.compact
+      end
 
-
-        new_type = parse_new_type
-
-        @current_function << new_type unless @current_type.empty?
-        @current_type = []
-        final_function = transform_function_tokens(@current_function)
-
-        # Distinguis between function without arguments:
-        #   -> unit => [:<, 'unit']
-        # and generic type without function (e.g. in the  type parameter of a class Array.('Array[Integer]'))
-        #   Array[Integer] => ['Array', {:type ... }]
-        if @current_function.at(0) != :< && final_function.size == 1
-          final_function.last
-        else
-          final_function
+      def variable_group?(token_group)
+        token_group.drop(1).all? do |token|
+          token.is_a?(Hash) &&
+            (token[:kind] == :type_var || token[:kind] == :generic_type)
         end
       end
 
-      private
+      def transform_nested_function(token_group)
+        if token_group.size > 1 &&
+           variable_group?(token_group) &&
+           token_group[0].is_a?(String)
+          { :type => token_group.first, :parameters => token_group.drop(1), :kind => :generic_type }
+        else
+          token_group.first
+        end
+      end
+    end
 
+    module TypeProcessor
       def parse_start_of_type
         new_type = parse_new_type
         @current_function << new_type unless @current_type.empty?
         @stack << @current_function
         @current_type = []
         @current_function = []
-      end
-
-      def transform_function_tokens(tokens)
-        next_function_elem, last_token = tokens.inject([[],[]]) do |(total,type), token|
-            if token == :<
-              total << type
-              [total, []]
-            else
-              type << token
-              [total,type]
-            end
-          end
-        next_function_elem << last_token
-        next_function_elem = next_function_elem.map do |token_group|
-          if(token_group.is_a?(Array))
-            if (token_group.size > 1 &&
-                token_group.drop(1).all? do |token|
-                  token.is_a?(Hash) && (token[:kind] == :type_var || token[:kind] == :generic_type)
-                end &&
-                token_group[0].is_a?(String))
-              { :type => token_group.first, :parameters => token_group.drop(1), :kind => :generic_type }
-            else
-              token_group.first
-            end
-          elsif(token_group.is_a?(String))
-            token_group
-          end
-        end
-        next_function_elem.compact
       end
 
       def parse_end_of_function
@@ -134,18 +82,7 @@ module TypedRb
       end
 
       def parse_end_of_binding
-        new_type = parse_new_type
-        @current_function << new_type unless @current_type.empty?
-        bound = if @current_function.size == 1
-                  { :type => @current_function.first, :kind => :type_var }
-                else
-                  if @binding.nil?
-                    # This is the case for nested generic types
-                    { :type => @current_function.first, :parameters => @current_function.drop(1), :kind => :generic_type }
-                  else
-                    { :type => @current_function.first, :bound => @current_function.last, :binding => @binding, :kind => :type_var }
-                  end
-                end
+        bound = parse_variable_binding
         # method variables
         if bound[:kind] == :type_var && method_info[bound[:type]]
           bound = method_info[bound[:type]].dup
@@ -158,7 +95,24 @@ module TypedRb
         @current_type = []
       end
 
+      def parse_variable_binding
+        new_type = parse_new_type
+        @current_function << new_type unless @current_type.empty?
+        if @current_function.size == 1
+          { :type => @current_function.first, :kind => :type_var }
+        else
+          if @binding.nil?
+            # This is the case for nested generic types
+            { :type => @current_function.first, :parameters => @current_function.drop(1), :kind => :generic_type }
+          else
+            { :type => @current_function.first, :bound => @current_function.last, :binding => @binding, :kind => :type_var }
+          end
+        end
+      end
+
       def parse_next_elem
+        return parse_binding if @in_binding
+
         new_type = parse_new_type
         @current_function << new_type unless @current_type.empty?
         @current_function << :<
@@ -170,12 +124,86 @@ module TypedRb
         new_type = :unit if new_type == 'unit'
         if new_type.to_s.end_with?('...')
           new_type = new_type.split('...').first
-          if new_type.nil?
-            new_type = @current_function.pop
-          end
+          new_type = @current_function.pop if new_type.nil?
           new_type =  { :type => 'Array', :kind => :rest, :parameters => [new_type] }
         end
         new_type
+      end
+    end
+
+    class Parser
+      include TokenProcessor
+      include TypeProcessor
+
+      def self.parse(expr, method_info = {})
+        (@parser || Parser.new(method_info)).parse(expr)
+      end
+
+      attr_reader :method_info
+
+      def initialize(method_info = {})
+        @method_info = method_info
+        @current_type = []
+        @current_function = []
+        @stack = []
+        @in_binding = false
+      end
+
+      def parse(expr)
+        expr = sanitize_input(expr)
+        expr.each_char { |elem| parse_next_char(elem) }
+        build_final_signature
+      end
+
+      private
+
+      def sanitize_input(expr)
+        expr.gsub(/\s+/, '').gsub('->', '/')
+      end
+
+      PARSERS = {
+        '(' => ->(parser) { parser.parse_start_of_type },
+        '[' => ->(parser) { parser.parse_start_of_type },
+        ')' => ->(parser) { parser.parse_end_of_function },
+        '<' => ->(parser) { parser.parse_binding },
+        '>' => ->(parser) { parser.parse_binding },
+        ']' => ->(parser) { parser.parse_end_of_binding },
+        '/' => ->(parser) { parser.parse_next_elem }
+      }
+
+      def parse_next_char(elem)
+        setup_binding_context(elem)
+        if PARSERS[elem]
+          PARSERS[elem][self]
+        else
+          @current_type << elem
+        end
+      end
+
+      def setup_binding_context(elem)
+        @in_binding = true  if elem == '['
+        @in_binding = false if elem == ']'
+        @binding = elem     if elem == '<' || elem == '>'
+      end
+
+      def build_final_signature
+        fail ParsingError.new(expr, 'Unbalanced parentheses.') unless @stack.empty?
+
+        new_type = parse_new_type
+
+        @current_function << new_type unless @current_type.empty?
+        @current_type = []
+        final_function = transform_function_tokens(@current_function)
+
+        # Distinguis between function without arguments:
+        #   -> unit => [:<, 'unit']
+        # and generic type without function (e.g. in the  type parameter of a class Array.('Array[Integer]'))
+        #   Array[Integer] => ['Array', {:type ... }]
+        if @current_function.at(0) != :< && final_function.size == 1
+          final_function.last
+        else
+          final_function
+        end
       end
     end
   end
